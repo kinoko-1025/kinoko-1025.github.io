78ee30bfb2b3956a49fd260f566199063be23053
kinoko-1025 <ks.1025@outlook.jp>
2026-09-08 11:45:19 +0900

    test03

--- Audio Analyzer/index.js
+++ Audio Analyzer/index.js
@@ -3,19 +3,30 @@ const number = document.getElementById("number");
 
 
 // 表示する数値
-let value = audioBuffer.getChannelData(0);
+let value = 123456
 
 
 // 画面に表示
 number.textContent = value;
 
 
-fileInput.addEventListener("change", (event) => {
+fileInput.addEventListener("change", async (event) => {
 
     const file = event.target.files[0];
 
     if (!file) return;
+    
+    console.log(file.name);
+    console.log(file.size);
+    console.log(file.type);
+    
+    const arrayBuffer = await file.arrayBuffer();
+    const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+    const data = audioBuffer.getChannelData(0);
+    
+    concole.log(data);
+    console.log(audioBuffer);
+    
 
-    console.log(file);
 
 });
